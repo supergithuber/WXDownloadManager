f2381f3b0940308182e68c448c2a1df8b860564a
sivanWu <1984430988@qq.com>
2018-05-25 10:50:25 +0800

    support framework and carthag

---
+++ DownloadManager/DownloadManager.h
@@ -0,0 +1,19 @@
+//
+//  DownloadManager.h
+//  DownloadManager
+//
+//  Created by HFY on 2018/5/25.
+//  Copyright © 2018年 Wuxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DownloadManager.
+FOUNDATION_EXPORT double DownloadManagerVersionNumber;
+
+//! Project version string for DownloadManager.
+FOUNDATION_EXPORT const unsigned char DownloadManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DownloadManager/PublicHeader.h>
+
+#import <DownloadManager/WXDownloadManager.h>
